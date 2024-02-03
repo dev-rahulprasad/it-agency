9dd5b33e548990bed0a2d12e42375daf6a5a939c
Rahul Prasad <147226959+dev-rahulprasad@users.noreply.github.com>
2024-02-03 22:17:33 +0530

    Our Services page added

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
 // menu-toggle start
+const body = document.querySelector("body");
 const menuBtn = document.querySelectorAll(".menuToggle");
 const overlay = document.querySelector(".overlay");
 const menuSlide = document.querySelector(".nav-menu");
 const toggleSlide = ()=>{
+    body.classList.toggle("overflow-hidden");
     menuSlide.classList.toggle("active");
     overlay.classList.toggle("active");
 }
